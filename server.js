595a9022988c2cb214ec0fe79580d98dc6d183e0
Alexander Rindö <alexander.rindo@dn.se>
2019-01-03 16:45:47 +0100

    Further mysql config update

--- server.js
+++ server.js
@@ -97,8 +97,6 @@ app.get('/', function (req, res) {
 
 app.get('/pagecount', function (req, res) {
   const con = mysql.createConnection({
-    host: '127.0.0.1',
-    port: '3306',
     user: 'admin',
     password: '100%Widgets',
     database: 'quickshot'
